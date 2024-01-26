92a7b100e7ecb44540dd958366c95b520fe665a0
rehan mulakhel <rehan.mulakhel@epfl.ch>
2024-01-27 00:44:10 +0100

    Add previous/next links below chapter.

---
+++ src/components/ChapterPrevNext.vue
@@ -0,0 +1,69 @@
+<script setup>
+const props = defineProps({
+  chapters: {
+    type: Array,
+    required: true,
+  },
+  current: {
+    type: String,
+    required: true,
+  },
+});
+
+import { ref, watchEffect, toRaw } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const prevSlug = ref(null);
+const nextSlug = ref(null);
+
+watchEffect(() => {
+  const chapters = structuredClone(toRaw(props.chapters));
+  const index = chapters.findIndex((c) => c.slug === props.current);
+  if (index === -1) {
+    prevSlug.value = null;
+    nextSlug.value = null;
+    return;
+  }
+  for (let i = index - 1; i >= 0; --i) {
+    const chapter = chapters[i];
+    if (chapter.slug) {
+      prevSlug.value = chapter.slug;
+      break;
+    }
+  }
+  for (let i = index + 1; i < chapters.length; ++i) {
+    const chapter = chapters[i];
+    if (chapter.slug) {
+      nextSlug.value = chapter.slug;
+      break;
+    }
+  }
+});
+</script>
+
+<template>
+  <aside class="prev-next">
+    <RouterLink v-if="prevSlug" :to="prevSlug">
+      <div>Précédent</div>
+    </RouterLink>
+    <RouterLink v-if="nextSlug" :to="nextSlug">
+      <div>Suivant</div>
+    </RouterLink>
+  </aside>
+</template>
+
+<style scoped>
+.prev-next {
+  display: flex;
+  justify-content: center;
+}
+.prev-next a {
+  border: none;
+}
+.prev-next div {
+  background-color: #bf8d32;
+  color: #110d0d;
+  margin: 8px;
+  padding: 8px;
+}
+</style>
--- src/views/ChapterView.vue
+++ src/views/ChapterView.vue
@@ -1,19 +1,23 @@
 <script setup>
-import { ref, watchEffect } from 'vue';
+import { inject, ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 import { useFetch } from '../fetch.js';
 import ChapterDocument from '../components/ChapterDocument.vue';
+import ChapterPrevNext from '../components/ChapterPrevNext.vue';
 
 const route = useRoute();
 
+const chapterSlug = ref(null);
 const chapterUrl = ref(null);
 
 watchEffect(() => {
   const path = `${route.params.course}/${route.params.chapter}`;
   chapterUrl.value = `https://app.lexdocta.com/api/courses/${path}`;
+  chapterSlug.value = route.params.chapter;
 });
 
-const { data, error } = useFetch(chapterUrl);
+const { data: course } = inject('course');
+const { data: chapter, error } = useFetch(chapterUrl);
 
 function process(content) {
   const url = `https://app.lexdocta.com/api/courses/${route.params.course}/`;
@@ -24,8 +28,12 @@ function process(content) {
 <template>
   <div class="chapter">
     <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
-    <div v-else-if="data">
-      <ChapterDocument :name="data.name" :content="process(data.content)" />
+    <div v-else-if="course && chapter">
+      <ChapterDocument
+        :name="chapter.name"
+        :content="process(chapter.content)"
+      />
+      <ChapterPrevNext :chapters="course.chapters" :current="chapterSlug" />
     </div>
     <div v-else>Loading...</div>
   </div>
